4f241b11a1eeb8ec3d296da6c0f4970ac3925013
Michael302355301 <302355301@student.rocmondriaan.nl>
2023-02-15 14:28:13 +0100

    nav footer

---
+++ README.md
@@ -0,0 +1,19 @@
+# sd21-p7-pizzaproject
+
+Beste student, 
+
+Hierbij de repository van het Pizza Project. 
+Dit project maak je individueel. 
+Je leert het PHP framework Symfony. 
+
+Zet in je repository ook je: 
+- Functioneel ontwerp
+- Technisch ontwerp
+- JosF test inloggen
+
+Vergeet niet te werken met de Developer Branch. 
+Voor elke User Story maak je een aparte branch aan. 
+
+Ook wordt je deze periode beoordeeld op je gedrag. 
+
+SUCCES!!! 
---
+++ src/Controller/PizzaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PizzaController extends AbstractController
+{
+
+
+    #[Route('/')]
+    public function home(): Response
+    {
+        return $this->render('bezoeker/home.html.twig');
+    }
+
+    #[Route('contact')]
+    public function contact(): Response
+    {
+        $contact = "";
+        return $this->render('bezoeker/defaults/navbar.html.twig',['contact'=>$contact]);
+    }
+
+}
