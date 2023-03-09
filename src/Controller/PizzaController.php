ff8e1de52a3b1485164041532caf91d0f8979bc3
Michael302355301 <102236953+Michael302355301@users.noreply.github.com>
2023-03-09 14:16:45 +0100

    tttt

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Controller;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Category;
+use Doctrine\Persistence\ManagerRegistry;
 
 class PizzaController extends AbstractController
 {
@@ -21,6 +23,16 @@ class PizzaController extends AbstractController
         }
         return $this->render('bezoeker/home.html.twig', ['category' => $category]);
     }
+
+    #[Route('/category/{id}', name: 'cat_prod')]
+    public function category(Category $category, ProductRepository $productRepository ): Response
+    {
+        $product = $productRepository->findBy(['category' => $category]);
+
+        return $this->render('bezoeker/product.html.twig',
+            ['products' => $product,
+                'id' => $category]);
+        }
 
     #[Route('contact')]
     public function contact(): Response
---
+++ src/Entity/Product.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    private ?string $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $img = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(string $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
